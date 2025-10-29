d413fdd13d6cfc5d91f0fa27ad885fd4241183bf
Carlos <carferher8@alu.edu.gva.es>
2025-10-29 14:42:39 +0100

    Clases personaje creada

--- src/main/java/org/example/Personajes/Personaje.java
+++ src/main/java/org/example/Personajes/Personaje.java
@@ -1,4 +1,14 @@
 package org.example.Personajes;
 
+import org.example.Dialogos.Dialogos;
+import org.example.Objetos.Tipo;
+
 public class Personaje {
+    private String nombre;
+    private String dialogo;
+
+    public Personaje(String nombre, String dialogo) {
+        this.nombre = nombre;
+        this.dialogo = Dialogos.getDialogos().stream().filter(s -> s.getNombre().equals(nombre)).findFirst().get().getDialogo();
+    }
 }
