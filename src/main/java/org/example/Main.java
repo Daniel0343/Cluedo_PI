ca8601e398efe2439873193dee488bcd293561ac
hector <hecbasgil@alu.edu.gva.es>
2025-10-31 12:03:34 +0100

    Algunos cambios en main por hacer pruebas, clases de Personaje, jugador, habitacion empezado  y movimiento

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,16 @@
 package org.example;
 
-import org.example.Dialogos.Dialogos;
-import org.example.Objetos.Descripcion;
-import org.example.Objetos.Descripciones;
-
-import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        Habitacion.crearInfo();
+        Jugador j = new Jugador("nombre", Habitacion.habitaciones[0]);
 
-
-
-        // Dialogos.getDialogos().forEach(d -> System.out.println(d.getNombre()+" "+d.getDialogo()+" "+d.getTipo()));
+        while (true) {
+            j.acciones();
+        }
     }
+
 }
