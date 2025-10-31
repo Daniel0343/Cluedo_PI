a041ad2e865134310f1ed7df92b2973a60286452
Daniel0343 <danpasmar9@alu.edu.gva.es>
2025-10-31 11:59:33 +0100

    Dialogos, puntuacion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,10 +10,9 @@ import java.util.ArrayList;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
 
-        Dialogos.getDialogos().forEach(d -> System.out.println(d.getNombre()+" "+d.getDialogo()+" "+d.getTipo()));
+
+        // Dialogos.getDialogos().forEach(d -> System.out.println(d.getNombre()+" "+d.getDialogo()+" "+d.getTipo()));
     }
 }
