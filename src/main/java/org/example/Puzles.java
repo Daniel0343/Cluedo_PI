8d4640f36fbb37a589759f202cdfdda62d1c7053
Carlos <carferher8@alu.edu.gva.es>
2025-11-05 14:49:00 +0100

    Crear puzles

--- src/main/java/org/example/Puzles.java
+++ src/main/java/org/example/Puzles.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Puzles {
     public static boolean puzle1(){
 
@@ -7,6 +10,12 @@ public class Puzles {
     }
 
     public static boolean puzle2(){
+        ArrayList<String> resultado = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            resultado.add("Enrredadera "+i);
+        }
+        Collections.shuffle(resultado);
+
 
         return  true;
     }
