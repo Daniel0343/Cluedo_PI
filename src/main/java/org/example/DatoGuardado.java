7a16efd2c55576c97c2349141cbcc38a7f3b0e87
Carlos <carferher8@alu.edu.gva.es>
2025-11-05 14:26:58 +0100

    Función de guardado de inventario y grabadora

---
+++ src/main/java/org/example/DatoGuardado.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class DatoGuardado {
+    private String nombre;
+    private int puntos;
+    private String sala;
+
+    public DatoGuardado(String nombre, int puntos, String sala) {
+        this.nombre = nombre;
+        this.puntos = puntos;
+        this.sala = sala;
+    }
+
+
+
+}
--- src/main/java/org/example/Guardado.java
+++ src/main/java/org/example/Guardado.java
@@ -1,5 +1,59 @@
 package org.example;
 
-public class Guardado {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.example.Dialogos.Grabadora;
+import org.example.Salas.Sala;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Guardado {
+    public void guardarPartida(String nombre, int puntos, String sala, Grabadora grabadora, Inventario inventario) {
+        DatoGuardado datoGuardado = new DatoGuardado(nombre, puntos, sala);
+        guardarGrabadora(grabadora);
+        guardarInventario(inventario);
+    }
+
+    public void guardarGrabadora(Grabadora grabadora) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter("guardado/grabadora.json")) {
+            gson.toJson(grabadora, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Grabadora leerGrabadora(){
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader("guardado/grabadora.json")) {
+            Grabadora grabadora = gson.fromJson(reader, Grabadora.class);
+            return grabadora;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public void guardarInventario(Inventario inventario) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter("guardado/inventario.json")) {
+            gson.toJson(inventario, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Inventario leerInventario(){
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader("guardado/inventario.json")) {
+            Inventario inventario = gson.fromJson(reader, Inventario.class);
+            return inventario;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public void guardarSalas(){}
+    public Sala leerSala(){return null;}
 }
